679b16a2637ba4034011ff097f65b97df75bc8b1
Developer <faustogarciajacinto@gmail.com>
2022-06-18 23:36:40 -0300

    add style

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
+import {BsCodeSlash} from 'react-icons/bs'
 export default function Header() {
     return (
         <div id="Header">
             <div className="logo">
+                <BsCodeSlash className="iconLogo"/>
                 <h1>Fausto Garcia Jacinto Silva</h1>
             </div>
             <div className="menu">
