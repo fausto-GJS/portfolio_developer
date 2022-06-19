1525ea980dd47da608426c1337de30bfb0a45251
Developer <faustogarciajacinto@gmail.com>
2022-06-18 21:03:21 -0300

    add pages

--- client/src/components/common/Router.js
+++ client/src/components/common/Router.js
@@ -1,10 +1,24 @@
-
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './Header'
+import AboutUs from '../pages/AboutUs'
+import Contact from '../pages/Contact'
+import Portfolio from '../pages/Portfolio'
+import Error from '../pages/Error'
+import Footer from './Footer'
+export default function Router() {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route index exact path='/' element={<AboutUs />} />
+        <Route exact path='/portfolio' element={<Portfolio/>}/>
+        <Route exact path='/contact' element={<Contact/>}/>
+        <Route exact path='*' element={<Error/>}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+
 }
 
-export default App;
+
---
+++ client/src/components/pages/Error.js
@@ -0,0 +1,7 @@
+export default function Error(){
+    return(
+        <div id="Error">
+            Error
+        </div>
+    )
+}
