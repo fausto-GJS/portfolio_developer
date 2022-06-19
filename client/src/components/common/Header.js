466bcea915114ec472ef95ce368f17e70aa7d0e0
Developer <faustogarciajacinto@gmail.com>
2022-06-18 22:53:34 -0300

    add style

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,7 +1,15 @@
-export default function Header(){
-    return(
+import { Link } from 'react-router-dom'
+export default function Header() {
+    return (
         <div id="Header">
-            Header
+            <div className="logo">
+                <h1>Fausto Garcia Jacinto Silva</h1>
+            </div>
+            <div className="menu">
+                <Link to='/'>Sobre</Link>
+                <Link to='/portfolio'>Portfolio</Link>
+                <Link to='/contact'>Contato</Link>
+            </div>
         </div>
     )
 }
